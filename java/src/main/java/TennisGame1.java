32b6521ccfba251cba197eff0baa6c40486ba147
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-11 02:14:37 +0530

    Removing bloaters - Long method using IDE refactor - Extract method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -19,58 +19,76 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
+        String score;
         if (firstPlayerScore==secondPlayerScore)
         {
-            switch (firstPlayerScore)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = getScoreIfEqual();
         }
         else if (firstPlayerScore>=4 || secondPlayerScore>=4)
         {
-            int minusResult = firstPlayerScore-secondPlayerScore;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getScoreForLongerGame();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = firstPlayerScore;
-                else { score+="-"; tempScore = secondPlayerScore;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = getScoreForNormalGame();
         }
         return score;
     }
+
+	public String getScoreForNormalGame() {
+		String score="";
+		int tempScore=0;
+		for (int i=1; i<3; i++)
+		{
+		    if (i==1) tempScore = firstPlayerScore;
+		    else { score+="-"; tempScore = secondPlayerScore;}
+		    switch(tempScore)
+		    {
+		        case 0:
+		            score+="Love";
+		            break;
+		        case 1:
+		            score+="Fifteen";
+		            break;
+		        case 2:
+		            score+="Thirty";
+		            break;
+		        case 3:
+		            score+="Forty";
+		            break;
+		    }
+		}
+		return score;
+	}
+
+	public String getScoreForLongerGame() {
+		String score;
+		int minusResult = firstPlayerScore-secondPlayerScore;
+		if (minusResult==1) score ="Advantage player1";
+		else if (minusResult ==-1) score ="Advantage player2";
+		else if (minusResult>=2) score = "Win for player1";
+		else score ="Win for player2";
+		return score;
+	}
+
+	public String getScoreIfEqual() {
+		String score;
+		switch (firstPlayerScore)
+		{
+		    case 0:
+		            score = "Love-All";
+		        break;
+		    case 1:
+		            score = "Fifteen-All";
+		        break;
+		    case 2:
+		            score = "Thirty-All";
+		        break;
+		    default:
+		            score = "Deuce";
+		        break;
+		    
+		}
+		return score;
+	}
 }
